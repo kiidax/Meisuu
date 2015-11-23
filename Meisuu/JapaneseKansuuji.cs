4932e5e2234fd885480909c9d2f398d79b30cb4c
Katsuya Iida <kiidax@outlook.com>
2015-11-23 13:50:02 +0900

    Unit testing.

--- Meisuu/JapaneseKansuuji.cs
+++ Meisuu/JapaneseKansuuji.cs
@@ -8,13 +8,16 @@ namespace Meisuu
 {
     public class JapaneseKansuuji
     {
+        public const ulong UseArabicNumbers = 1 << 0;
+        public const ulong UseDaijiNumbers = 1 << 1;
+
         private const string SuujiChars = "一二三四五六七八九";
         private const string SmallKuraiChars = "十百千";
         private static readonly decimal[] Pow10Values;
         private static readonly decimal[] Pow10000Values;
         private const string DaijiSuujiChars = "壱弐参";
         private const string DaijiSmallKuraiChars = "拾";
-        private const string LargeKuraiChars = "万億兆京垓𥝱";
+        private const string LargeKuraiChars = "万億兆京垓杼穣";
         private const char ZeroChar = '零';
 
         static JapaneseKansuuji()
@@ -26,7 +29,7 @@ namespace Meisuu
                 v *= 10;
                 Pow10Values[i] = v;
             }
-            Pow10000Values = new decimal[4];
+            Pow10000Values = new decimal[7];
             v = 1;
             for (int i = 0; i < Pow10000Values.Length; i++)
             {
@@ -35,16 +38,72 @@ namespace Meisuu
             }
         }
 
-        public static bool TryParse(string s, out decimal result)
+        /// <summary>
+        /// Converts the Japanese Kansuuji representation of a number to decimal equivalent.
+        /// </summary>
+        /// <remarks>
+        /// decimal.MaxValue is 79228162514264337593543950335 or about 7穣9千𥝱.
+        /// Any greater or equal value than 1溝 throws an FormatException.
+        /// Otherwise any greater value than decimal.MaxValue throws an OverflowException.
+        /// Use of arabic characters like "9億" is not supported at the moment.
+        /// Use of 大字 like "弐億" is not supported at the moment.
+        /// </remarks>
+        /// <param name="s">A string containing a number to convert.</param>
+        /// <param name="style">The OR-ed styles of Kansuuji.</param>
+        /// <returns>The result of conversion.</returns>
+        /// <exception cref="ArgumentException">s is null</exception>
+        /// <exception cref="FormatException">s is not in the correct format.</exception>
+        /// <exception cref="OverflowException">s represents a number greater than decimal.MaxValue.</exception>
+        public static decimal Parse(string s, ulong style)
+        {
+            decimal result;
+            int endIndex;
+            if (!TryParseInternal(s, style, out endIndex, out result)) throw new FormatException();
+            if (s.Length != endIndex) throw new FormatException();
+            return result;
+        }
+
+        /// <summary>
+        /// Converts the Japanese Kansuuji representation of a number to decimal equivalent.
+        /// </summary>
+        /// <param name="s">A string containing a number to convert.</param>
+        /// <param name="result">The result of conversion.</param>
+        /// <returns>true if s was converted successfully; otherwise, false.</returns>
+        public static bool TryParse(string s, ulong style, out decimal result)
         {
             int endIndex;
-            if (!TryParse(s, 0, out endIndex, out result)) return false;
+            if (!TryParse(s, style, out endIndex, out result)) return false;
             if (endIndex != s.Length) return false;
             return true;
         }
 
-        public static bool TryParse(string s, decimal style, out int endIndex, out decimal result)
+        /// <summary>
+        /// Converts the Japanese Kansuuji representation of a number to decimal equivalent.
+        /// </summary>
+        /// <param name="s">A string containing a number to convert.</param>
+        /// <param name="style">The OR-ed styles of Kansuuji.</param>
+        /// <param name="endIndex"></param>
+        /// <param name="result">The result of conversion.</param>
+        /// <returns>true if s was converted successfully; otherwise, false.</returns>
+        public static bool TryParse(string s, ulong style, out int endIndex, out decimal result)
         {
+            try
+            {
+                return TryParseInternal(s, style, out endIndex, out result);
+            }
+            catch (OverflowException)
+            {
+                endIndex = 0;
+                result = 0;
+                return false;
+            }
+        }
+
+        public static bool TryParseInternal(string s, ulong style, out int endIndex, out decimal result)
+        {
+            if (s == null) throw new ArgumentNullException();
+            if ((style & UseArabicNumbers) != 0) throw new NotSupportedException();
+            if ((style & UseDaijiNumbers) != 0) throw new NotSupportedException();
             endIndex = 0;
             result = 0;
             if (s.Length > 0 && s[0] == ZeroChar)
--- MeisuuTest/MeisuuTest.cs
+++ MeisuuTest/MeisuuTest.cs
@@ -11,22 +11,22 @@ namespace MeisuuTest
         public void Under100()
         {
             decimal result;
-            Assert.IsFalse(JapaneseKansuuji.TryParse("", out result));
-            Assert.IsTrue(JapaneseKansuuji.TryParse("零", out result));
+            Assert.IsFalse(JapaneseKansuuji.TryParse("", 0L, out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("零", 0L, out result));
             Assert.AreEqual(0m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("一", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("一", 0L, out result));
             Assert.AreEqual(1m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("二", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("二", 0L, out result));
             Assert.AreEqual(2m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("十", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("十", 0L, out result));
             Assert.AreEqual(10m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("十三", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("十三", 0L, out result));
             Assert.AreEqual(13m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("一十", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("一十", 0L, out result));
             Assert.AreEqual(10m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四十", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四十", 0L, out result));
             Assert.AreEqual(40m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四十五", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四十五", 0L, out result));
             Assert.AreEqual(45m, result);
         }
 
@@ -34,19 +34,19 @@ namespace MeisuuTest
         public void Under1000()
         {
             decimal result;
-            Assert.IsTrue(JapaneseKansuuji.TryParse("百", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("百", 0L, out result));
             Assert.AreEqual(100m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("百三", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("百三", 0L, out result));
             Assert.AreEqual(103m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("一百", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("一百", 0L, out result));
             Assert.AreEqual(100m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四百", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四百", 0L, out result));
             Assert.AreEqual(400m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四百五", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四百五", 0L, out result));
             Assert.AreEqual(405m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("百六十", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("百六十", 0L, out result));
             Assert.AreEqual(160m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四百七十", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四百七十", 0L, out result));
             Assert.AreEqual(470m, result);
         }
 
@@ -54,21 +54,21 @@ namespace MeisuuTest
         public void Under10000()
         {
             decimal result;
-            Assert.IsTrue(JapaneseKansuuji.TryParse("千", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("千", 0L, out result));
             Assert.AreEqual(1000m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("千三", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("千三", 0L, out result));
             Assert.AreEqual(1003m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("一千", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("一千", 0L, out result));
             Assert.AreEqual(1000m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四千", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四千", 0L, out result));
             Assert.AreEqual(4000m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四千五", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四千五", 0L, out result));
             Assert.AreEqual(4005m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("千六十", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("千六十", 0L, out result));
             Assert.AreEqual(1060m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四千七十", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四千七十", 0L, out result));
             Assert.AreEqual(4070m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四千七百", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四千七百", 0L, out result));
             Assert.AreEqual(4700m, result);
         }
 
@@ -76,55 +76,89 @@ namespace MeisuuTest
         public void Under10m()
         {
             decimal result;
-            Assert.IsFalse(JapaneseKansuuji.TryParse("万", out result));
-            Assert.IsFalse(JapaneseKansuuji.TryParse("万三", out result));
-            Assert.IsTrue(JapaneseKansuuji.TryParse("一万", out result));
+            Assert.IsFalse(JapaneseKansuuji.TryParse("万", 0L, out result));
+            Assert.IsFalse(JapaneseKansuuji.TryParse("万三", 0L, out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("一万", 0L, out result));
             Assert.AreEqual(10000m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四万", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四万", 0L, out result));
             Assert.AreEqual(40000m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四万五百", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四万五百", 0L, out result));
             Assert.AreEqual(40500m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("六十万七千", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("六十万七千", 0L, out result));
             Assert.AreEqual(607000m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("八千九十万百", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("八千九十万百", 0L, out result));
             Assert.AreEqual(80900100m, result);
-            Assert.IsFalse(JapaneseKansuuji.TryParse("九十八千万", out result));
+            Assert.IsFalse(JapaneseKansuuji.TryParse("九十八千万", 0L, out result));
         }
 
         [TestMethod]
         public void Under10t()
         {
             decimal result;
-            Assert.IsFalse(JapaneseKansuuji.TryParse("兆", out result));
-            Assert.IsFalse(JapaneseKansuuji.TryParse("兆三", out result));
-            Assert.IsTrue(JapaneseKansuuji.TryParse("一兆", out result));
+            Assert.IsFalse(JapaneseKansuuji.TryParse("兆", 0L, out result));
+            Assert.IsFalse(JapaneseKansuuji.TryParse("兆三", 0L, out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("一兆", 0L, out result));
             Assert.AreEqual(1000000000000m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四兆", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四兆", 0L, out result));
             Assert.AreEqual(4000000000000m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四兆五百", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四兆五百", 0L, out result));
             Assert.AreEqual(4000000000500m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("六十兆七千", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("六十兆七千", 0L, out result));
             Assert.AreEqual(60000000007000m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("八千九十兆百", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("八千九十兆百", 0L, out result));
             Assert.AreEqual(8090000000000100m, result);
-            Assert.IsFalse(JapaneseKansuuji.TryParse("九十八千兆", out result));
+            Assert.IsFalse(JapaneseKansuuji.TryParse("九十八千兆", 0L, out result));
         }
 
         [TestMethod]
         public void Under10t2()
         {
             decimal result;
-            Assert.IsFalse(JapaneseKansuuji.TryParse("京一万", out result));
-            Assert.IsFalse(JapaneseKansuuji.TryParse("三京万", out result));
-            Assert.IsTrue(JapaneseKansuuji.TryParse("一京二億", out result));
+            Assert.IsFalse(JapaneseKansuuji.TryParse("京一万", 0L, out result));
+            Assert.IsFalse(JapaneseKansuuji.TryParse("三京万", 0L, out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("一京二億", 0L, out result));
             Assert.AreEqual(10000000200000000m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("四京五百億", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("四京五百億", 0L, out result));
             Assert.AreEqual(40000050000000000m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("六十京七千億", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("六十京七千億", 0L, out result));
             Assert.AreEqual(600000700000000000m, result);
-            Assert.IsTrue(JapaneseKansuuji.TryParse("八千九十京百億", out result));
+            Assert.IsTrue(JapaneseKansuuji.TryParse("八千九十京百億", 0L, out result));
             Assert.AreEqual(80900000010000000000m, result);
-            Assert.IsFalse(JapaneseKansuuji.TryParse("九十八千京億", out result));
+            Assert.IsFalse(JapaneseKansuuji.TryParse("九十八千京億", 0L, out result));
+        }
+
+        [TestMethod]
+        public void MaxValue()
+        {
+            decimal result;
+            Assert.IsTrue(JapaneseKansuuji.TryParse("七穣九千杼三垓", 0L, out result));
+            Assert.AreEqual(79000000300000000000000000000m, result);
+            Assert.IsFalse(JapaneseKansuuji.TryParse("八穣", 0L, out result));
+            try { JapaneseKansuuji.Parse("八穣", 0L); Assert.Fail(); } catch (OverflowException) { }
+            try { JapaneseKansuuji.Parse("八千穣", 0L); Assert.Fail(); } catch (OverflowException) { }
+            try { JapaneseKansuuji.Parse("七穣九千三百杼", 0L); Assert.Fail(); } catch (OverflowException) { }
+            try { JapaneseKansuuji.Parse("七穣九千二百二十八杼千六百二十五垓千四百二十六京四千三百三十七兆五千九百三十五億四千三百九十五万三百三十六", 0L); Assert.Fail(); } catch (OverflowException) { }
+            Assert.IsTrue(JapaneseKansuuji.TryParse("七穣九千二百二十八杼千六百二十五垓千四百二十六京四千三百三十七兆五千九百三十五億四千三百九十五万三百三十五", 0L, out result));
+            Assert.AreEqual(79228162514264337593543950335m, result);
+        }
+
+        [TestMethod]
+        public void Wrapper()
+        {
+            decimal result;
+            try { JapaneseKansuuji.Parse("", 0L); Assert.Fail(); } catch (FormatException) { }
+            try { JapaneseKansuuji.Parse("数", 0L); Assert.Fail(); } catch (FormatException) { }
+            Assert.AreEqual(56m, JapaneseKansuuji.Parse("五十六", 0L));
+            Assert.IsFalse(JapaneseKansuuji.TryParse("五十六数", 0L, out result));
+            try { JapaneseKansuuji.Parse("五十六数", 0L); Assert.Fail(); } catch (FormatException) { }
+        }
+
+        [TestMethod]
+        public void NotSupportedYet()
+        {
+            decimal result;
+            try { JapaneseKansuuji.TryParse("", JapaneseKansuuji.UseArabicNumbers, out result); Assert.Fail(); } catch (NotSupportedException) { }
+            try { JapaneseKansuuji.TryParse("", JapaneseKansuuji.UseDaijiNumbers, out result); Assert.Fail(); } catch (NotSupportedException) { }
         }
     }
 }
